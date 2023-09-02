4e53bb3201a731d886d2305c573f71da364934d6
Sean Michael <seanlasono@gmail.com>
2023-09-02 03:08:50 +0300

    added navbar
    issue: colormode keeps changing to light on initialization even tho default is dark

---
+++ src/app/providers.tsx
@@ -0,0 +1,28 @@
+// app/providers.tsx
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
+
+export function Providers({ 
+    children 
+  }: { 
+  children: React.ReactNode 
+  }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  )
+}
