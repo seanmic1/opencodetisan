23f05651e76af9a12db602cc43a28df356130f27
Sean Michael <seanlasono@gmail.com>
2023-09-04 22:26:56 +0300

    updated dashboard to not use layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from "./providers"
+import { usePathname } from 'next/navigation';
 
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
@@ -17,6 +20,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  if (['/dashboard'].includes(usePathname())){
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers>
+            {children}
+          </Providers>
+          </body>
+      </html>
+    )
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
