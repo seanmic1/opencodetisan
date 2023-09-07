4fd3e853bda43580e8d27c201cb2152a8feb23e1
Sean Michael <seanlasono@gmail.com>
2023-09-07 16:34:36 +0300

    added animation and fixes to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,9 +9,21 @@ import {
   Button,
   Icon,
   IconProps,
+  useColorModeValue,
+  Center,
+  keyframes
 } from '@chakra-ui/react'
 
 import {TriangleUpIcon} from '@chakra-ui/icons'
+import { SiVercel } from 'react-icons/si'
+import { motion } from 'framer-motion';
+
+const animationKeyframes = keyframes`
+  0% { background-position: 0 0; }
+  100% { background-position: 100% 0; }
+`;
+
+const animation = `${animationKeyframes} 4s alternate infinite`;
 
 export default function Hero() {
   return (
@@ -29,7 +41,11 @@ export default function Hero() {
             <Text>
               Develop.
             </Text>
-            <Text>
+            <Text
+              bgGradient={'linear(to-l, #7928CA, #FF0080)'}
+              bgSize={'300%'}
+              bgClip={'text'}
+              animation={animation}>
               Preview.
             </Text>
             <Text>
@@ -40,23 +56,24 @@ export default function Hero() {
         <Text color={'#888'} maxW={'3xl'} fontSize={'1.25rem'} fontWeight={'400'}>
         Vercel's frontend cloud gives developers the frameworks, workflows, and infrastructure to build a faster, more personalized Web.
         </Text>
-        <Stack spacing={6} direction={'row'}>
+        <Stack spacing={6} direction={'row'} align={'top'}>
           <Button
+            h={'48px'}
             as={'a'}
             href='/dashboard'
-            rounded={'md'}
-            px={3}
+            rounded={'lg'}
+            px={6}
             colorScheme={'orange'}
-            bg={'white'}
-            _hover={{ bg: 'gray.200' }}
-            leftIcon={<TriangleUpIcon/>}>
+            bg={useColorModeValue('black', 'white')}
+            _hover={{'bg': useColorModeValue('gray.500', 'gray.400')}}
+            leftIcon={<SiVercel></SiVercel>}>
             Start Deploying
           </Button>
-          <Flex rounded={'md'} bgGradient={'linear(to-r, blue.400, cyan)'} p={'1px'}>
-            <Button rounded={'md'} px={6} bg={'black'} border={'transparent'}>
+          <Center rounded={'lg'} bgGradient={'linear(to-r, blue.400, cyan)'} h={'48px'} p={'0.5'}>
+            <Button as={'a'} px={10} h={'full'} w={'full'} rounded={'lg'} bg={useColorModeValue('white','black')} _hover={{'bg':'transparent'}}>
               Get a Demo
             </Button>
-          </Flex>
+          </Center>
         </Stack>
       </Stack>
     </Container>
