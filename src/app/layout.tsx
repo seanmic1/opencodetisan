ff9711624e6d0cd2944e2a57be21f7e40dcadb96
seanmic1 <seanlasono@gmail.com>
2023-09-10 11:57:01 +0300

    fixed metadata export issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from "./providers"
@@ -7,6 +5,8 @@ import { usePathname } from 'next/navigation';
 
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+
+import ClientComponent from './client'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,28 +20,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
-  if (['/dashboard'].includes(usePathname())){
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <Providers>
-            {children}
-          </Providers>
-          </body>
-      </html>
-    )
-  }
-
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          <Navbar/>
-          {children}
-          <Footer/>
-        </Providers>
-        </body>
-    </html>
+    <ClientComponent>
+      {children}
+    </ClientComponent>
   )
 }
